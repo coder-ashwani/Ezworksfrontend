c74509e3093374160115cc3acd2975d51a262b40
coder-ashwani <ashwaniagarwal333@gmail.com>
2025-03-15 23:26:54 +0530

    added form  design

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,5 @@
 const Card = ({ image, title, description }) => {
   return (
-    // <div className="p-4 mt-4 ml-4  border-2 border-black rounded-md h-[150px] w-[170px]">
-    //   <img src={image} alt={title} className="w-[40px] h-[40px] object-cover" />
-    //   <h2 className="text-xl font-bold">{title}</h2>
-    //   <p>{description}</p>
-    // </div>
     <div className=" container p-3  mt-6  ml-4   border-2 border-black rounded-md h-[170px] w-[180px] bg-[#112949] ">
       <div className="flex items-center pb-1">
         <img
